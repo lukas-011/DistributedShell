fcbec8d5d913d14b2264719dbb6de4e07545fed4
PJBeans <phillip236.po@gmail.com>
2023-12-05 14:21:08 -0500

    Delete binToText and clean up mainProg

--- mainProg.c
+++ mainProg.c
@@ -1,12 +1,5 @@
-#include <bits/types/FILE.h>
 #include <stdio.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <malloc.h>
-#include "binToText.h"
-#include <pthread.h>
+
 
 #define BUFFER_SMALL 32
 #define BUFFER_MID 64
